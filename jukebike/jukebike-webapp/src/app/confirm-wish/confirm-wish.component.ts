a18a58632107a121b198c22ddf5f1db369d6e0bc
Paul Maria Bartusch <paulbartusch@gmx.de>
2020-08-02 23:00:09 +0200

    give mins and secs as confirmation info to the user

--- jukebike/jukebike-webapp/src/app/confirm-wish/confirm-wish.component.ts
+++ jukebike/jukebike-webapp/src/app/confirm-wish/confirm-wish.component.ts
@@ -9,13 +9,19 @@ import { JukeBikeService } from '../jukebike.service'
 })
 export class ConfirmWishComponent implements OnInit {
 
+  queuePosition = -1
+  readableMinutes = -1
+  readableSeconds = -1
+
   constructor(
     private jukeBikeService: JukeBikeService
   ) { }
 
   ngOnInit(): void {
+    let currentConfirmation = this.jukeBikeService.currentConfirmation
+    this.queuePosition = currentConfirmation.queuePosition
+    this.readableMinutes = Math.floor(currentConfirmation.secondsToWait / 60)
+    this.readableSeconds = currentConfirmation.secondsToWait % 60
   }
-
-  currentConfirmation = this.jukeBikeService.currentConfirmation
 
 }
